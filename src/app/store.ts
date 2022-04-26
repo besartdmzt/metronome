81849430199befc2df2b9e1bfd8d804380d535f3
BesartD <besart.domazet@technoperia.com>
2022-04-26 23:07:13 +0200

    Remove the counter

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,12 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import counterReducer from "../features/counter/counterSlice";
 import messageReducer from "src/app/message-slice";
+import songsReducer from "src/app/songs-slice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     message: messageReducer,
+    songs: songsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== "production",
