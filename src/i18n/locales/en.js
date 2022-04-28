6affd5b26b6341bf0fbb8a459a5cd94f9277d3c8
BesartD <besart.domazet@technoperia.com>
2022-04-28 02:33:30 +0200

    Remove unused texts

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -3,10 +3,6 @@ export const resources = {
     translation: {
       TITLE: "DIGITAL METRONOME",
       SONGS_THAT_USE_THIS_BPM: "Songs that use this BPM",
-      ERROR: {
-        TITLE: "Ouch!",
-        SOMETHING_WENT_WRONG: "Cannot load the songs, try again later!",
-      },
     },
   },
 };
