0789dc64371b01eabba568da86f627b647f6d55b
BesartD <besart.domazet@technoperia.com>
2022-04-29 11:29:24 +0200

    Show description on the songs

--- src/features/songslist/SongsList.tsx
+++ src/features/songslist/SongsList.tsx
@@ -1,8 +1,10 @@
 import { Box, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { RootState } from "src/app/store";
 
 const SongsList = () => {
+  const { t } = useTranslation();
   const filteredSongs = useSelector(
     (state: RootState) => state.songs.filteredSongs
   );
@@ -16,9 +18,13 @@ const SongsList = () => {
       flexDirection="column"
       paddingTop="2rem"
     >
+      <Typography pb="0.5rem" variant="MN14Blue">
+        {t("SONGS_THAT_USE_THIS_BPM")}:
+      </Typography>
+
       {filteredSongs.map((song, index) => {
         return (
-          <Typography key={index} pb="0.5rem" variant="MN14Blue">
+          <Typography key={index} pb="0.5rem" variant="MN14Grey">
             {song.name} {song.artist}
           </Typography>
         );
